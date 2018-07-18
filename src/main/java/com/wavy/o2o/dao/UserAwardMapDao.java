69aaed18417c37e3c6dd8e29d1d3ee99a6615add
WavyPeng <w1a9v9y3@gmail.com>
2018-07-19 00:57:18 +0800

    实现UserAwardMapDap

--- src/main/java/com/wavy/o2o/dao/UserAwardMapDao.java
+++ src/main/java/com/wavy/o2o/dao/UserAwardMapDao.java
@@ -19,4 +19,33 @@ public interface UserAwardMapDao {
     List<UserAwardMap> queryUserAwardMapList(@Param("userAwardCondition")UserAwardMap userAwardCondition,
                                              @Param("rowIndex")int rowIndex,
                                              @Param("pageSize")int pageSize);
+
+
+    /**
+     * 配合queryUserAwardMapList返回相同查询条件下的兑换奖品记录数
+     * @param userAwardMapCondition
+     * @return
+     */
+    int queryUserAwardMapCount(@Param("userAwardCondition")UserAwardMap userAwardMapCondition);
+
+    /**
+     * 根据userAwardId返回某条奖品兑换信息
+     * @param userAwardId
+     * @return
+     */
+    UserAwardMap queryUserAwardMapById(long userAwardId);
+
+    /**
+     * 添加一条奖品兑换信息
+     * @param userAwardMap
+     * @return
+     */
+    int insertUserAwardMap(UserAwardMap userAwardMap);
+
+    /**
+     * 更新奖品兑换信息，主要更新奖品领取状态
+     * @param userAwardMap
+     * @return
+     */
+    int updateUserAwardMap(UserAwardMap userAwardMap);
 }
