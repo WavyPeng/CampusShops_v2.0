4498602df7397602b8c8a8b2a8d6482a8d8221b1
WavyPeng <w1a9v9y3@gmail.com>
2018-07-22 00:23:24 +0800

    添加二维码工具类

--- src/main/java/com/wavy/o2o/util/CodeUtil.java
+++ src/main/java/com/wavy/o2o/util/CodeUtil.java
@@ -1,6 +1,15 @@
 package com.wavy.o2o.util;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 验证码工具类
@@ -21,5 +30,32 @@ public class CodeUtil {
             return false;
         }
         return true;
+    }
+
+    /**
+     * 生成二维码图片流
+     * @param content
+     * @param response
+     * @return
+     */
+    public static BitMatrix generateQRCodeStream(String content, HttpServletResponse response){
+        // 给响应添加头部信息，告知浏览器返回图片流
+        response.setHeader("Cache-Control","no-store");
+        response.setHeader("Pragma","no-cache");
+        response.setDateHeader("Expires",0);
+        response.setContentType("image/png");
+        // 设置图片的文字编码以及内边框距
+        Map<EncodeHintType,Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET,"UTF-8");
+        hints.put(EncodeHintType.MARGIN,0);
+        BitMatrix bitMatrix;
+        try{
+            // 参数：编码内容、编码类型、生成图片宽度、生成图片高度、设置参数
+            bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE,300,300,hints);
+        }catch (WriterException e){
+            e.printStackTrace();
+            return null;
+        }
+        return bitMatrix;
     }
 }
